c662eadad401ba99d74392c965fe4b0484e5e1eb
gabay <gabay@cs.technion.ac.il>
2019-10-01 22:16:35 +0300

    added setup.py and gitignore

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='sudoku',
+      version='1.0',
+      description='Sudoku solver',
+      url='https://github.com/gabay/sudoku',
+      author='Roi Gabay',
+      author_email='roigby@gmail.com',
+      license='GPLv3',
+      packages=['sudoku'],
+      install_requires=[
+          'z3-solver', 'opencv-python', 'flask'
+      ])
+#dependency: z3, opencv, flask
